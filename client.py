02073e43c3594dbddd0f033f694cd16a806d651e
ihanken <ihanken@bellsouth.net>
2017-03-27 20:55:51 -0500

    Added new tests and class structures.

--- client.py
+++ client.py
@@ -1,2 +1,22 @@
-def helloWorld():
-    return "Hello, world!"
+class Client():
+    def __init__(self, ip, port):
+        self.__ip = ip
+        self.__port = port
+
+    ''' Getters '''
+    @property
+    def ip(self):
+        return self.__ip
+
+    @property
+    def port(self):
+        return self.__port
+
+    ''' Setters '''
+    @ip.setter
+    def ip(self, ip):
+        self.__ip = ip
+
+    @port.setter
+    def port(self, port):
+        self.__port = port
--- tests/test_client.py
+++ tests/test_client.py
@@ -2,9 +2,36 @@ import unittest
 from context import client
 
 class TestClient(unittest.TestCase):
-    def setUp(self): pass
+    def setUp(self):
+        self._clientOne = client.Client("192.168.1.1", 1)
+        self._clientTwo = client.Client("192.168.1.2", 2)
+        self._clientThree = client.Client("192.168.1.3", 3)
 
-    def tearDown(self): pass
+    def tearDown(self):
+        del self._clientOne
+        del self._clientTwo
+        del self._clientThree
 
-    def testHelloWorld(self):
-        self.assertEqual(client.helloWorld(), "Hello, world!")
+    def testGetters(self):
+        self.assertEqual(self._clientOne.ip, "192.168.1.1")
+        self.assertEqual(self._clientTwo.ip, "192.168.1.2")
+        self.assertEqual(self._clientThree.ip, "192.168.1.3")
+
+        self.assertEqual(self._clientOne.port, 1)
+        self.assertEqual(self._clientTwo.port, 2)
+        self.assertEqual(self._clientThree.port, 3)
+
+    def testSetters(self):
+        self._clientOne.ip = self._clientTwo.ip
+        self._clientThree.ip = self._clientOne.ip
+
+        self._clientOne.port = self._clientThree.port
+        self._clientTwo.port = self._clientOne.port
+
+        self.assertEqual(self._clientOne.ip, self._clientThree.ip)
+        self.assertEqual(self._clientThree.ip, self._clientTwo.ip)
+        self.assertEqual(self._clientTwo.ip, self._clientOne.ip)
+
+        self.assertEqual(self._clientOne.port, self._clientThree.port)
+        self.assertEqual(self._clientThree.port, self._clientTwo.port)
+        self.assertEqual(self._clientTwo.port, self._clientOne.port)
