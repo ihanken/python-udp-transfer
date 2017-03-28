6701612236b441aab85c3766c149cf25b7f45e16
Ian Hanken <ihanken@bellsouth.net>
2017-03-28 15:00:55 -0500

    Class creation (#3)
    * Travis commit.
    
    * Forgot to save the yaml file.
    
    * Updated travis yaml.
    
    * Added coverage as a requirement.
    
    * Updated tests.
    
    * Added new tests and class structures.
    
    * Finished the skeleton for client.py
    
    * Finished the creation of the Server class.

--- client.py
+++ client.py
@@ -1,2 +1,46 @@
-def helloWorld():
-    return "Hello, world!"
+from socket import *
+from sys import *
+import struct
+from constants import *
+
+class Client():
+    def __init__(self, args):
+        ''' Locally Assigned Attributes '''
+        self.__args = args
+        self.__buffer = 1024
+
+        ''' Command Line Argument Attributes '''
+        self.__ip = self.parseIPArgument() # Parse our IP.
+        self.__port = self.parsePortArgument() # Parse our port.
+        self.__address = (self.__ip, self.__port) # Create a tuple from the values.
+
+        print(self.__address)
+
+        self.__socket = socket(AF_INET, SOCK_DGRAM) # Set up our socket.
+
+    ''' Parse the IP address. Fails if no IP is provided. '''
+    def parseIPArgument(self):
+        try: return str(self.__args[1])
+        except IndexError:
+            exit("Please provide an IP address.\n" + CLIENT_EXAMPLE_USAGE)
+
+    ''' Parse the port. Fails if no value is provided or the value is invalid. '''
+    def parsePortArgument(self):
+        try: port = int(self.__args[2])
+        except ValueError: # Can't convert the value to an int.
+            exit("Port must be an integer.\n" + CLIENT_EXAMPLE_USAGE)
+        except IndexError: # No value was provided.
+            exit("Port number must be provided.\n" + CLIENT_EXAMPLE_USAGE)
+
+        if not 1024 <= port <= 60000: # Port is out of range.
+            exit("Port must be between 1024 and 60000.\n" + CLIENT_EXAMPLE_USAGE)
+        else: return port
+
+    ''' Request the given filename from the server '''
+    def requestFile(self, filename): return filename
+
+    ''' Loop and allow socket to receive. '''
+    def beginReceiving(self, bytes = 1024): return bytes
+
+if __name__ == "__main__":
+    client = Client(argv)
--- tests/test_server.py
+++ tests/test_server.py
@@ -2,9 +2,31 @@ import unittest
 from context import server
 
 class TestServer(unittest.TestCase):
-    def setUp(self): pass
+    def setUp(self):
+        self.__serverOne = server.Server(["server.py", "192.168.1.1", 4000])
 
-    def tearDown(self): pass
+    def tearDown(self):
+        del self.__serverOne
 
-    def testHelloWorld(self):
-        self.assertEqual(server.helloWorld(), "Hello, world!")
+    def testNoArgs(self):
+        with self.assertRaises(IndexError) and self.assertRaises(SystemExit):
+            server.Server(["server.py"])
+
+    def testNoPort(self):
+        with self.assertRaises(IndexError) and self.assertRaises(SystemExit):
+            server.Server(["server.py", "192.168.1.1"])
+
+    def testOutOfRangePort(self):
+        with self.assertRaises(SystemExit):
+            server.Server(["server.py", "192.168.1.1", 1])
+
+    def testStringForPort(self):
+        with self.assertRaises(ValueError) and self.assertRaises(SystemExit):
+            server.Server(["server.py", "192.168.1.1", "Test"])
+
+    def testValidInitialization(self):
+        server.Server(["server.py", "192.168.1.1", 4000])
+
+    def testSendFile(self):
+        self.assertEqual(self.__serverOne.sendFile("file.txt"), "file.txt")
+        self.assertNotEqual(self.__serverOne.sendFile("file1.txt"), "file.txt")
