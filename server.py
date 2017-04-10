5ba4358589990916dc7386f47fc437935106f978
Ian Hanken <ihanken@bellsouth.net>
2017-04-09 20:13:07 -0500

    Alternating bit (#4)
    * Travis commit.
    
    * Finished alternating bit.
    
    * Removed the time package.
    
    * Commented a bit more.

--- server.py
+++ server.py
@@ -10,23 +10,59 @@ class Server():
         self.__buffer = 1024
 
         ''' Command Line Argument Attributes '''
-        self.__ip = self.parseIPArgument() # Parse our IP.
-        self.__port = self.parsePortArgument() # Parse our port.
-        self.__address = (self.__ip, self.__port) # Create a tuple from the values.
+        self.__port = self.parsePortArgument()      # Parse our port.
+        self.__address = ("", self.__port)          # Create a tuple from the
+                                                    # values.
+        self.__socket = None # Set up the socket variable.
 
-        print(self.__address)
+    '''
+    This function allows the server to listen to requests through the
+    provided port.
+    '''
+    def beginListening(self):
+        self.__socket = socket(AF_INET, SOCK_STREAM) # Initialize our socket.
+        self.__socket.bind(self.__address) # Bind the socket to the address.
+        self.__socket.listen(1) # Specify a window size of 1.
 
-        self.__socket = socket(AF_INET, SOCK_DGRAM) # Set up our socket.
+        print("Listening")
 
-    ''' Parse the IP address. Fails if no IP is provided. '''
-    def parseIPArgument(self):
-        try: return str(self.__args[1])
-        except IndexError:
-            exit("Please provide an IP address.\n" + SERVER_EXAMPLE_USAGE)
+        while True:
+            connectionSocket, _ = self.__socket.accept() # Begin listening
+
+            sequenceNumber = 1 # A record of our current sequence number.
+            i = 0 # The data index.
+            data = ["I", "love", "to", "socket", "program."] # Our data to send.
+
+            while True:
+                ACK = connectionSocket.recv(1024).decode("utf-8") # Receive data.
+
+                if i >= len(data): # The last piece of data has been sent.
+                    # Create the closing message.
+                    closing = CLOSING_MESSAGE + '^' + str(sequenceNumber)
+
+                    connectionSocket.send(closing.encode("utf-8")) # Send closing.
+                    connectionSocket.close() # Close the connection
+                    break # Break our infinite loop.
+                elif str(sequenceNumber) == ACK: # We can move on.
+                    sequenceNumber ^= 1 # Invert our sequence number.
+
+                    # Create the next packet.
+                    packet = data[i] + '^' + str(sequenceNumber)
+                    connectionSocket.send(packet.encode("utf-8")) # Send packet.
+
+                    i += 1 # Increment the data index.
+                else: # Resend the message.
+                    # Create the next packet.
+                    packet = data[i] + '^' + str(sequenceNumber)
+                    connectionSocket.send(packet.encode("utf-8")) # Send packet.
+
+            # We're finished.
+            print("Sent \"{}\" to receiver.".format(' '.join(data)))
+
 
     ''' Parse the port. Fails if no value is provided or the value is invalid. '''
     def parsePortArgument(self):
-        try: port = int(self.__args[2])
+        try: port = int(self.__args[1])
         except ValueError: # Can't convert the value to an int.
             exit("Port must be an integer.\n" + SERVER_EXAMPLE_USAGE)
         except IndexError: # No value was provided.
@@ -41,3 +77,5 @@ class Server():
 
 if __name__ == "__main__":
     server = Server(argv)
+
+    server.beginListening()
--- tests/test_server.py
+++ tests/test_server.py
@@ -3,29 +3,17 @@ from context import server
 
 class TestServer(unittest.TestCase):
     def setUp(self):
-        self.__serverOne = server.Server(["server.py", "192.168.1.1", 4000])
+        self.__serverOne = server.Server(["server.py", 4000])
 
     def tearDown(self):
         del self.__serverOne
 
     def testNoArgs(self):
-        with self.assertRaises(IndexError) and self.assertRaises(SystemExit):
+        with self.assertRaises(ValueError) and self.assertRaises(SystemExit):
             server.Server(["server.py"])
 
-    def testNoPort(self):
-        with self.assertRaises(IndexError) and self.assertRaises(SystemExit):
-            server.Server(["server.py", "192.168.1.1"])
-
-    def testOutOfRangePort(self):
-        with self.assertRaises(SystemExit):
-            server.Server(["server.py", "192.168.1.1", 1])
-
-    def testStringForPort(self):
-        with self.assertRaises(ValueError) and self.assertRaises(SystemExit):
-            server.Server(["server.py", "192.168.1.1", "Test"])
-
     def testValidInitialization(self):
-        server.Server(["server.py", "192.168.1.1", 4000])
+        server.Server(["server.py", 4000])
 
     def testSendFile(self):
         self.assertEqual(self.__serverOne.sendFile("file.txt"), "file.txt")
