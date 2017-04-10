f78ac172ceec55b35308181c021be894ba6fb641
Ian Hanken <ihanken@bellsouth.net>
2017-04-09 20:18:15 -0500

    Selective repeat (#5)
    * Travis commit.
    
    * Finished alternating bit.
    
    * Removed the time package.
    
    * Commented a bit more.
    
    * Finished selective repeat.

--- alternating-bit-turnin/client.py
+++ alternating-bit-turnin/client.py
@@ -3,7 +3,6 @@ from sys import *
 import struct
 from constants import *
 import errno
-import re
 
 class Client():
     def __init__(self, args):
@@ -19,10 +18,9 @@ class Client():
 
     '''
     This function requests the data that the server is holding. The server
-    is specified by the ip and port supplied to the client. Alternating bit
-    Protocol is used.
+    is specified by the ip and port supplied to the client.
     '''
-    def requestDataWithAlternatingBit(self):
+    def requestData(self):
         self.__socket = socket(AF_INET, SOCK_STREAM) # Initialize Socket.
         self.__socket.connect((self.__ip, self.__port)) # Connect socket.
         self.__socket.settimeout(2.0) # Give the socket a 2 second timeout.
@@ -58,50 +56,6 @@ class Client():
 
         return data
 
-    '''
-    This function requests the data that the server is holding. The server
-    is specified by the ip and port supplied to the client. Selective Repeat
-    Protocol is used.
-    '''
-    def requestDataWithSelectiveRepeat(self):
-        self.__socket = socket(AF_INET, SOCK_STREAM) # Initialize Socket.
-        self.__socket.connect((self.__ip, self.__port)) # Connect socket.
-        self.__socket.settimeout(2.0) # Give the socket a 2 second timeout.
-
-        data = [] # A record of the data received.
-
-        _ = input("Press enter to begin transmission.") # Prompt user to begin.
-        print("Requesting data from server...\n")
-
-        ACK = None # Send an empty ACK to start transmission.
-
-        while True:
-            try:
-                self.__socket.send(("ACK: " + str(ACK)).encode("utf-8")) # Set up the connection.
-                received = self.__socket.recv(1024).decode("utf-8") # Get data.
-
-                received = received.split('^') # Split to get the message and ACK.
-
-                if len(received) < 2: continue  # This is an empty message,
-                                                # so skip to the next one.
-
-                ACK = int(re.sub("[^0-9]", "", received[1])) # Get the number we need to ACK.
-                message = received[0] # Only grab first message.
-
-                if message == CLOSING_MESSAGE: break # Stop if we have all data.
-                else: # Keep ACKing
-                    while len(data) - 1 < ACK: data.append(None) # Extend the list.
-                    data[ACK] = message # Add the message to the list.
-
-                    self.__socket.send(("ACK: " + str(ACK)).encode("utf-8")) # Send ACK
-            except timeout: print("Timed out.") # Handle a time out.
-            except error: break # Prevents a discontinued connection from
-                                # breaking the script.
-
-        self.__socket.close() # Close the socket.
-
-        return data
-
     ''' Parse the IP address. Fails if no IP is provided. '''
     def parseIPArgument(self):
         try: return str(self.__args[1])
@@ -128,7 +82,7 @@ class Client():
 
 if __name__ == "__main__":
     client = Client(argv)
-    data = client.requestDataWithSelectiveRepeat()
+    data = client.requestData()
 
     print("Raw data: {}".format(data))
     print("Joined data: \"{}\"".format(' '.join(data)))
