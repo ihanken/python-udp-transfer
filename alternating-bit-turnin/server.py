ca10a0f2fd64026f65c65571d9bc325b346b8ca9
ihanken <ihanken@bellsouth.net>
2017-04-09 20:08:14 -0500

    Finished selective repeat.

--- alternating-bit-turnin/server.py
+++ alternating-bit-turnin/server.py
@@ -2,7 +2,6 @@ from socket import *
 from sys import *
 import struct
 from constants import *
-import re
 
 class Server():
     def __init__(self, args):
@@ -18,9 +17,9 @@ class Server():
 
     '''
     This function allows the server to listen to requests through the
-    provided port. It uses alternating bit as the protocol to send data.
+    provided port.
     '''
-    def beginListeningWithAlternatingBit(self):
+    def beginListening(self):
         self.__socket = socket(AF_INET, SOCK_STREAM) # Initialize our socket.
         self.__socket.bind(self.__address) # Bind the socket to the address.
         self.__socket.listen(1) # Specify a window size of 1.
@@ -60,61 +59,6 @@ class Server():
             # We're finished.
             print("Sent \"{}\" to receiver.".format(' '.join(data)))
 
-    '''
-    This function allows the server to listen to requests through the
-    provided port. It uses alternating bit as the protocol to send data.
-    '''
-    def beginListeningWithSelectiveRepeat(self):
-        self.__socket = socket(AF_INET, SOCK_STREAM) # Initialize our socket.
-        self.__socket.bind(self.__address) # Bind the socket to the address.
-        self.__socket.listen(1) # Specify a window size of 1.
-
-        print("Listening")
-
-        while True:
-            connectionSocket, _ = self.__socket.accept() # Begin listening
-
-            data = ["I", "love", "to", "socket", "program.", "It", "is", "the",
-                    "best", "type", "of", "programming."
-            ] # Our data to send.
-
-            ackedIndices = set()
-
-            while len(ackedIndices) < len(data):
-                currentData = [(data[i], i) for i in range(len(data))
-                                if i not in ackedIndices]
-
-                currentData = currentData[:max(4, len(currentData))]
-
-                for datum in currentData:
-                    # Create the next packet.
-                    packet = datum[0] + '^' + str(datum[1])
-                    connectionSocket.send(packet.encode("utf-8")) # Send packet.
-
-                    ACK = connectionSocket.recv(1024).decode("utf-8") # Receive data
-
-                    ACK = ACK.split()[1]
-
-                    try:
-                        ACK = int(re.sub("[^0-9]", "", ACK))
-
-                        if 0 <= int(ACK) < len(data):
-                            print("Adding ACK {}".format(int(ACK)))
-                            ackedIndices.add(int(ACK))
-                    except ValueError: pass
-
-                    #print(ackedIndices)
-
-            # We're finished.
-            print("Sent {} to the receiver.".format(' '.join(data)))
-
-            print(ackedIndices)
-
-            # Create the closing message.
-            closing = CLOSING_MESSAGE + '^' + str(len(data))
-
-            connectionSocket.send(closing.encode("utf-8")) # Send closing.
-            connectionSocket.close() # Close the connection
 
     ''' Parse the port. Fails if no value is provided or the value is invalid. '''
     def parsePortArgument(self):
@@ -134,4 +78,4 @@ class Server():
 if __name__ == "__main__":
     server = Server(argv)
 
-    server.beginListeningWithSelectiveRepeat()
+    server.beginListening()
